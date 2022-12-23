b9dc2fea1875f1b5905f363443b93fb5380316c5
Dan Gowans <d.gowans@cityssm.on.ca>
2022-12-23 10:09:41 -0500

    add function description

--- index.d.ts
+++ index.d.ts
@@ -19,8 +19,15 @@ interface FontAwesomeV5Icon {
     voted?: boolean;
 }
 
+/**
+ * Returns a promise with a full list of actual Font Awesome v5 icons.
+ */
 export declare function getList(): Promise<FontAwesomeV5Icon[]>;
 
+/**
+ * Returns a promise with a full list of Font Awesome icons with needed keys only.
+ * @param arrayOfNeededKeys 
+ */
 export declare function getListByKeys(
     arrayOfNeededKeys: FontAwesomeV5Keys[]
 ): Promise<FontAwesomeV5Icon[]>;
